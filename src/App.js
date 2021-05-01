41ed1e52e54e9ca8ce9f4be2ffcc4f487e62b20d
Ivan D. Sanchez <cosmosoft.soluciones@gmail.com>
2021-05-01 16:53:54 -0500

    add sidebar

--- src/App.js
+++ src/App.js
@@ -76,8 +76,8 @@ function App() {
         </a>
         <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
         <ul className="right valign-wrapper hide-on-med-and-down">
-          {menu.map((item)=>(
-            <li>
+          {menu.map((item, key)=>(
+            <li key={key}>
               <a href="#" onClick={()=>handleClick(item.name)}>
               {item.name}
               </a>
@@ -86,6 +86,15 @@ function App() {
         </ul>
       </nav>
     </header>
+    <ul ref={(menu => sidenav = menu)} className="sidenav" id="mobile-demo">
+      {menu.map((item, key)=>(
+        <li key={key}>
+          <a href="#" onClick={()=>handleClick(item.name)}>
+          {item.name}
+          </a>
+        </li>
+      ))}
+    </ul>
     <main className="container">
       <div className="card-panel">
         <section className="container">
@@ -104,24 +113,24 @@ function App() {
           <div className="row section">
             <div className="input-field col s12 m6 ">
               <input id='name' value={name} onChange={(e)=>setName(e.target.value)} type="text" className="validate" required/>
-              <label for='name'>Nombre Completo</label>
+              <label htmlFor='name'>Nombre Completo</label>
               <span className="helper-text" data-error="Campo requerido" />
             </div>    
             <div className="input-field col s12 m6 ">
               <input id='email' value={email} onChange={(e)=>setEmail(e.target.value)} type="email" className="validate" required/>
-              <label for='email'>Email</label>
+              <label htmlFor='email'>Email</label>
               <span className="helper-text" data-error="Ingrese un email válido" />
             </div>           
           </div>
           <div className="row section">
             <div className="input-field col s12 m6 ">
               <input id='phone' value={phone} onChange={(e)=>setPhone(e.target.value)} type="text" className="validate" required/>
-              <label for='phone'>Celular</label>
+              <label htmlFor='phone'>Celular</label>
               <span className="helper-text" data-error="Campo requerido" />
             </div>    
             <div className="input-field col s12 m6 ">
               <input id='age' value={age} onChange={(e)=>setAge(e.target.value)} type="number" className="validate" required min={18} max={100} />
-              <label for='age'>Edad</label>
+              <label htmlFor='age'>Edad</label>
               <span className="helper-text" data-error="La edad debe estar entre los 18 y 100 años" />
             </div>           
           </div>
@@ -140,11 +149,11 @@ function App() {
       </div>
     </main>
     <div id="modal1" className="modal" ref={(modalBox => modal = modalBox)}>
+      <div style={{textAlign: 'center'}}>
+        <i  className="material-icons medium" style={{color:'green'}}>check</i>
+      </div>
       <div className="modal-content">
         <h5 className='center'>Tu información fue enviada con éxito<br/> Estaremos en contacto contigo</h5>
-      </div>
-      <div>
-        <i  className="material-icons center medium" style={{color:'green'}}>check</i>
       </div>
     </div>
    
